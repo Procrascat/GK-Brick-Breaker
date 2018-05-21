a2b12d0acdc86c19411044cb1edd587194d26ff0
Procrascat <khalil.luke@gmail.com>
2018-05-21 16:10:24 -0500

    Seperate files

---
+++ BBScript.js
@@ -0,0 +1,67 @@
+ ////Variables 
+ var canvas;
+ var canvasContext;
+ var BallX = 75;
+ var BallY = 75;
+ var BallXSpeed = -3;
+ var BallYSpeed = -3;
+ ////////
+ window.onload = function() {
+     canvas = document.getElementById('gameCanvas'); ////
+     canvasContext = canvas.getContext('2d'); ////
+
+     var Framerate = 60
+     setInterval(function() {
+         drawEverything();
+         moveEverything();
+    }, 1000/Framerate);
+ }
+
+ function BGcolor() {
+     canvasContext.fillStyle = 'black';
+     canvasContext.fillRect(0,0, canvas.width, canvas.height);
+
+ }
+
+ function Ball() {
+     canvasContext.fillStyle = 'white';
+     canvasContext.beginPath();
+     canvasContext.arc(BallX, BallY, 20, 0, Math.PI*2, true);
+     canvasContext.fill();
+ }
+
+ function drawEverything() {
+     //// Background 
+     BGcolor(); //
+     //// Ball Sprite
+     Ball();
+
+ }
+
+ function moveEverything() {
+     //// Ball movement
+     BallX += BallXSpeed;
+     BallY += BallYSpeed;
+
+     Bounds(); 
+
+ }
+
+ function Bounds() {
+     //// Boundaries 
+     if(BallX > canvas.width) {
+        BallX *= -1; ///
+     }
+
+     if(BallX < 0) {
+        BallX *= -1;///
+     }
+
+     if(BallY > canvas.height) {
+        BallX *= -1;////
+     }
+
+     if(BallY < 0) {
+        BallX *= -1;////
+     }
+ }
