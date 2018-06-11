81d2392c57542ef537269e0dc3bc095beedecc12
Procrascat <khalil.luke@gmail.com>
2018-06-11 17:47:32 -0500

    paddle collision breaks game

--- BBScript.js
+++ BBScript.js
@@ -1,14 +1,23 @@
  ////Variables 
  var canvas;
  var canvasContext;
- var BallX = 75;
- var BallY = 75;
- var BallXSpeed = -3;
- var BallYSpeed = -3;
+ var BallX = canvas.width/2; /////
+ var BallY = canvas.height/2;
+ var BallXSpeed = 3;
+ var BallYSpeed = 3;
+ var PXpos = 400;
+ var PYpos = 500;
+ var Pwide = 75;
  ////////
  window.onload = function() {
      canvas = document.getElementById('gameCanvas'); ////
      canvasContext = canvas.getContext('2d'); ////
+     
+     canvas.addEventListener('mousemove', function(evt) {
+        var mousePos = CalculateMousePos(evt); ////
+        PXpos = mousePos.x - (Pwidth/2); /// center of paddle
+    } );
+   
 
      var Framerate = 60
      setInterval(function() {
@@ -16,26 +25,45 @@
          moveEverything();
     }, 1000/Framerate);
  }
+ 
+ function CalculateMousePos(evt) {
+     var rect = canvas.getBoundingClientRect(), root = document.documentElement;
 
- function BGcolor() {
-     canvasContext.fillStyle = 'black';
-     canvasContext.fillRect(0,0, canvas.width, canvas.height);
+     // account for margins, canvas position on page, scroll amount, etc.
+     var mouseX = evt.clientX - rect.left - root.scrollLeft;
+     var mouseY = evt.clientY - rect.top - root.scrollTop;
+     
+     return {
+         x: mouseX,
+         y: mouseY
+     };
+ } 
 
- }
-
- function Ball() {
-     canvasContext.fillStyle = 'white';
+ function colorRect(Xpos, Ypos, BLength, BWidth, BColor) {
      canvasContext.beginPath();
-     canvasContext.arc(BallX, BallY, 20, 0, Math.PI*2, true);
+     canvasContext.fillStyle = BColor;
+     canvasContext.fillRect(Xpos, Ypos, BLength, BWidth); ///
+     
      canvasContext.fill();
  }
 
- function drawEverything() {
-     //// Background 
-     BGcolor(); //
-     //// Ball Sprite
-     Ball();
 
+
+ function Ball(CXpos, CYpos, Clength, CColor) {
+    canvasContext.beginPath();
+    canvasContext.fillStyle = CColor;
+     canvasContext.arc(CXpos, CYpos, Clength, 0, Math.PI*2, true); //
+    
+     canvasContext.fill(); 
+ }
+
+ function drawEverything() {
+    //// Background 
+        colorRect(0, 0, canvas.width, canvas.height,'black'); //
+    ///// Paddle
+        colorRect(PXpos, PYpos, Pwide, 10, 'white'); //
+    //// Ball Sprite
+        Ball(BallX, BallY, 10, 'white');     //     
  }
 
  function moveEverything() {
@@ -44,6 +72,10 @@
      BallY += BallYSpeed;
 
      Bounds(); ////
+
+     BallCollision();
+
+     
 
  }
 
@@ -58,10 +90,22 @@
      }
 
      if(BallY >= canvas.height) {
-        BallYSpeed *= -1; ////
+        BallReset(); ////
      }
 
      if(BallY <= 0) {
         BallYSpeed *= -1; ////
      }
  }
+
+ function BallReset() { 
+     BallX = canvas.width/2;
+     BallY = canvas.height/2;
+ }
+
+ function BallCollision() {
+     if(BallX <= Pwidth && BallX >= PXpos + Pwidth && BallY >= Pheight) {
+        BallYSpeed *= -1;
+
+     }
+ } 
